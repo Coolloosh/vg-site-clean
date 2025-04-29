98edfd748673991a7f4f7e277a9f4ecb637b89b2
adtrunzo <adtrunzo@udel.edu>
2025-04-29 14:07:01 -0400

    mobile hero fix contact3

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -62,7 +62,7 @@ export default function ContactPage() {
         subtitle={<span className="text-purple-400 text-2xl md:text-2xl italic tracking-wide drop-shadow-[0_0_25px_rgba(0,255,0,0.3)] animate-fade-in-slow opacity-90">whisper into the chaos...</span>}
       /* subtitle={<span className="text-green-400 text-xl md:text-2xl italic tracking-wide opacity-80">Whisper into the chaos...</span>}*/
         gradientClass="bg-gradient-to-b from-transparent via-black/30 to-black"
-        imageClass={`${isMobile ? "object-cover object-center brightness-50 scale-65 transition duration-[2000ms]"  : 'scale-105'} object-top brightness-50 transition duration-[2000ms]`}
+        imageClass={`${isMobile ? "object-center brightness-50 scale-65 transition duration-[2000ms]"  : 'scale-105'} object-top brightness-50 transition duration-[2000ms]`}
         minHeight={isMobile
           ? "min-h-[100vh]"
           : "h-screen"
