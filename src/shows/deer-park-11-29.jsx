cfbbd47734034deb3094de06940f9261da50ab9f
adtrunzo <adtrunzo@udel.edu>
2025-04-30 20:21:27 -0400

    setlist1

--- src/shows/deer-park-11-29.jsx
+++ src/shows/deer-park-11-29.jsx
@@ -27,13 +27,47 @@ export default function DeerParkShow() {
   const videos = gallery?.videos || [];
 
   const setlist = [
-    { title: "Intro / Feedback" },
-    { title: "Mutant Parade", link: "https://open.spotify.com/track/xxx1" },
-    { title: "Glitter Bomb", link: "https://open.spotify.com/track/xxx2" },
-    { title: "Lizard Queen" },
-    { title: "Fangs Out", link: "https://open.spotify.com/track/xxx3" },
-    { title: "Chaos Reigns" },
-    { title: "Encore: Cherry Pit Vortex", link: "https://open.spotify.com/track/xxx4" }
+   
+    { title: "Rock And Roll", link: "https://www.youtube.com/watch?v=QVKVcALMHoY" },
+    { title: "Teenage Dirtbag" },
+    { title: "All The Small Things" },
+    { title: "Enter Sandman", link: "https://www.youtube.com/watch?v=QVKVcALMHoY" },
+    { title: "Circling Vultures" },
+    { title: "Creep", link: "https://www.youtube.com/watch?v=QVKVcALMHoY" },
+    { title: "Teenagers" },
+    { title: "You Shook Me All Night Long" },
+    { title: "Gives You Hell" },
+    { title: "(Like It's) Cocaine", link: "https://www.youtube.com/watch?v=QVKVcALMHoY" },
+    { title: "Zombie" },
+    { title: "Creeping Death (ft. Tom)" },
+    { title: "Cigarette Daydreams" },
+    { title: "Air Raid" },
+    { title: "Times Like These" },
+    { title: "Sunday Morning",},
+    { title: "American Idiot" },
+    { title: "Nothing Left" },
+    { title: "War Pigs" },
+    { title: "Cult Of Personality" },
+    { title: "Flagpole Sitta" },
+    { title: "Beautiful Disaster",},
+    { title: "Kilby Girl" },
+    { title: "Sweet Home Alabama" },
+    { title: "Everybody Talks" },
+    { title: "FRM" },
+    { title: "Sex On Fire" },
+    { title: "Chop Suey",},
+    { title: "My Own Inferno" },
+    { title: "The Middle" },
+    { title: "Digital Lemons" },
+    { title: "Killing In The Name" },
+    { title: "One Last Breath" },
+    { title: "Higher" },
+    { title: "Everlong",},
+    { title: "Two Weeks Past" },
+    { title: "Walk" },
+    { title: "Basket Case" },
+    { title: "Encore: Bulls On Parade" }
+
   ];
 
   useEffect(() => {
