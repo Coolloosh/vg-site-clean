133491e7194468c1159ccd46ea62a2cdd60f1737
adtrunzo <adtrunzo@udel.edu>
2025-05-05 00:49:03 -0400

    in-person checkout5

--- src/InPersonCheckout.jsx
+++ src/InPersonCheckout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useCart } from '../CartContext';
+import { useCart } from './CartContext';
 import { useLocation, useNavigate } from 'react-router-dom';
-import MerchCatalog from '../MerchCatalog';
+import MerchCatalog from './MerchCatalog';
 
 export default function InPersonCheckout() {
   const { cart } = useCart();
